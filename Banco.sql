a3228d007472b32f90dacf69b6c0adeb299150e1
Lucas Brembati Weizenmann <lucas8bitbw@gmail.com>
2026-05-14 21:53:57 -0400

    script do banco (base)

---
+++ Banco.sql
@@ -0,0 +1,162 @@
+CREATE DATABASE estatisticas_dnd;
+USE estatisticas_dnd;
+
+CREATE TABLE tamanho (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    tamanho VARCHAR(10) NOT NULL
+);
+
+CREATE TABLE tipo (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    tipo VARCHAR(10)
+);
+
+CREATE TABLE subtipo (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    subtipo VARCHAR(10)
+);
+
+CREATE TABLE alinhamento (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    tipo VARCHAR(10)
+);
+
+CREATE TABLE deslocamento (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    deslocamento VARCHAR(10)
+);
+
+CREATE TABLE subdeslocamento (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    subdeslocamento VARCHAR(10)
+);
+
+CREATE TABLE pericias (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    pericia VARCHAR(10)
+);
+
+CREATE TABLE resistencias (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    resistencia VARCHAR(10)
+);
+
+CREATE TABLE vulnerabilidades (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    vulnerabilidade VARCHAR(10)
+);
+
+CREATE TABLE imunidades_dano (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    imunidade VARCHAR(10)
+);
+
+CREATE TABLE imunidades_condicao (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    imunidade VARCHAR(10)
+);
+
+CREATE TABLE sentidos (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    sentido VARCHAR(30)
+);
+
+CREATE TABLE idiomas (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    idioma VARCHAR(25)
+);
+
+CREATE TABLE habitats (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    habitat VARCHAR(10)
+);
+
+CREATE TABLE monstros (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(50),
+    tamanho_id INT,
+    FOREIGN KEY (tamanho_id) REFERENCES tamanho(id),
+    tipo_id INT,
+    FOREIGN KEY (tipo_id) REFERENCES tipo(id),
+    subtipo_id INT,
+    FOREIGN KEY (subtipo_id) REFERENCES subtipo(id),
+    alinhamento_id INT,
+    FOREIGN KEY (alinhamento_id) REFERENCES alinhamento(id),
+    classe_armadura INT,
+    pontos_vida INT,
+    iniciativa INT,
+    forca INT,
+    destreza INT,
+    constituicao INT,
+    inteligencia INT,
+    sabedoria INT,
+    carisma INT,
+    nivel_desafio VARCHAR(5),
+    link VARCHAR(250)
+);
+
+CREATE TABLE deslocamento_monstro (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    deslocamento_id INT,
+    FOREIGN KEY (deslocamento_id) REFERENCES deslocamento(id),
+    monstro_id INT,
+    FOREIGN KEY (monstro_id) REFERENCES monstro(id),
+    distancia INT
+);
+
+CREATE TABLE pericia_monstro (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    pericia_id INT,
+    FOREIGN KEY (pericia_id) REFERENCES pericia(id),
+    monstro_id INT,
+    FOREIGN KEY (monstro_id) REFERENCES monstro(id)
+);
+
+CREATE TABLE resistencia_monstro (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    resistencia_id INT,
+    FOREIGN KEY (resistencias_id) REFERENCES resistencias(id),
+    monstro_id INT,
+    FOREIGN KEY (monstro_id) REFERENCES monstro(id)
+);
+
+CREATE TABLE vulnerabilidade_monstro (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    vulnerabilidades_id INT,
+    FOREIGN KEY (vulnerabilidade_id) REFERENCES vulnerabilidades(id),
+    monstro_id INT,
+    FOREIGN KEY (monstro_id) REFERENCES monstro(id)
+);
+
+CREATE TABLE imunidade_dano_monstro (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    imunidade_id INT,
+    FOREIGN KEY (imunidade_id) REFERENCES imunidades_dano(id),
+    monstro_id INT,
+    FOREIGN KEY (monstro_id) REFERENCES monstro(id)
+);
+
+CREATE TABLE imunidade_condicao_monstro (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    imunidade_id INT,
+    FOREIGN KEY (imunidade_id) REFERENCES imunidades_condicao(id),
+    monstro_id INT,
+    FOREIGN KEY (monstro_id) REFERENCES monstro(id)
+);
+
+CREATE TABLE sentido_monstro (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    sentido_id INT,
+    FOREIGN KEY (sentido_id) REFERENCES sentidos(id),
+    monstro_id INT,
+    FOREIGN KEY (monstro_id) REFERENCES monstro(id),
+    distancia INT
+);
+
+CREATE TABLE idioma_monstro (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    idioma_id INT,
+    FOREIGN KEY (idioma_id) REFERENCES idiomas(id),
+    monstro_id INT,
+    FOREIGN KEY (monstro_id) REFERENCES monstro(id)
+);
